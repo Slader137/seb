00c1d98816d29f874ecc3a476307584c984440d6
Sebastian Silva <cinc.exe@gmail.com>
2024-12-06 14:13:44 -0300

    base de datos actualizada

--- cliente.sql
+++ cliente.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-12-2024 a las 21:13:29
+-- Tiempo de generación: 06-12-2024 a las 17:44:47
 -- Versión del servidor: 8.0.40
 -- Versión de PHP: 8.2.12
 
@@ -106,7 +106,22 @@ INSERT INTO `producto` (`id_producto`, `categoria`, `nombre`, `descripcion`, `pr
 (3, 'entradas', 'Matambre arrollado con rusa', 'Matambre relleno con verduras, huevo y especias acompañado de ensalada rusa', 2800),
 (4, 'sopas', 'Locro', 'Clásico guiso criollo con maíz blanco, zapallo, carne de cerdo y vaca, embutidos y especias.', 2500),
 (5, 'sopas', 'Sopa de humita cremosa:', 'Crema de maíz fresco con un toque de queso criollo.', 3000),
-(6, 'sopas', 'Sopa de lentejas y panceta', 'Lentejas cocidas lentamente con panceta ahumada, chorizo colorado y especias', 3500);
+(6, 'sopas', 'Sopa de lentejas y panceta', 'Lentejas cocidas lentamente con panceta ahumada, chorizo colorado y especias', 3500),
+(7, 'ensaladas', 'Ensalada Rusa', 'Papas, zanahorias, arvejas, mayonesa.', 770),
+(8, 'ensaladas', 'Ensalada Mixta', 'Lechuga, tomate, cebolla.', 500),
+(9, 'ensaladas', 'Ensalada de Repollo y Zanahoria', 'Repollo blanco, zanahoria rallada, vinagreta de mostaza.', 600),
+(10, 'platosfuertes', 'Asado', 'Carne (costillas, morcilla, chorizo, vacío, etc.)', 3000),
+(11, 'platosfuertes', 'Milanesa a la Napolitana', 'Milanesa de carne o de pollo, jamón, queso, salsa de tomate, y gratinada al horno', 4500),
+(12, 'platosfuertes', 'Bife de Chorizo', 'Corte de carne de res asado a la parrilla, acompañado de papas fritas o ensalada.', 1000),
+(13, 'platosfuertes', 'Pollo a la Parrilla', 'Pollo marinado con hierbas y limón, asado a la parrilla. Acompañado de papas fritas o ensalada', 750),
+(14, 'platosfuertes', 'Choripán con Chimichurri', 'Chorizo parrillero en pan, con salsa chimichurri.', 400),
+(15, 'platosfuertes', 'Canelones de Carne', 'Pasta rellena de carne molida y espinaca, bañada en salsa blanca y gratinada al horno.', 700),
+(16, 'especialidades', 'Fugazzeta Rellena', 'Masa rellena de cebolla y queso, con un toque de aceite de oliva y orégano, cocinada al horno.', 850),
+(17, 'especialidades', 'Pastel de Papa', 'Guiso de carne picada, papas y especias, cubierto con puré de papas y gratinado al horno.', 750),
+(18, 'postres', 'Flan Casero con Dulce de Leche y Crema', 'Flan de huevo acompañado de dulce de leche y crema chantilly.', 350),
+(19, 'postres', 'Budín de Pan', 'Postre de pan remojado en leche y azúcar, con un toque de vainilla y pasas de uva, servido con salsa de caramelo.', 450),
+(20, 'postres', 'Panqueque con Dulce de Leche', 'Panqueque relleno con dulce de leche, servido con azúcar glas y una bola de helado.', 350),
+(21, 'bebidas', 'Gaseosas', 'CocaCola,Fanta,Sprite.', 300);
 
 --
 -- Índices para tablas volcadas
@@ -139,7 +154,7 @@ ALTER TABLE `producto`
 -- AUTO_INCREMENT de la tabla `carrito`
 --
 ALTER TABLE `carrito`
-  MODIFY `id_carrito` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_carrito` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT de la tabla `cliente`
@@ -151,7 +166,7 @@ ALTER TABLE `cliente`
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `id_producto` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_producto` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- Restricciones para tablas volcadas
